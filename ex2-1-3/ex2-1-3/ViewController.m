ecf1b8d235b31d205798551c7c39a23a724c28b0
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-20 16:09:00 +0900

    Completed. #14

--- ex2-1-3/ex2-1-3/ViewController.m
+++ ex2-1-3/ex2-1-3/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+- (IBAction)onTap:(id)sender;
+@property (weak, nonatomic) IBOutlet UIButton *btn;
+
+@property NSArray *items;
 @end
 
 @implementation ViewController
@@ -17,6 +21,8 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  self.items = @[@"ika", @"ebi", @"salmon", @"hamachi", @"maguro"];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +30,9 @@
   // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+  int rnd = arc4random() % self.items.count;
+  UIImage *backgroundImage = [UIImage imageNamed:self.items[rnd]];
+  [self.btn setBackgroundImage:backgroundImage forState:UIControlStateNormal];
+}
 @end
