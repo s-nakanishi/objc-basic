b58dfa5a845955bf54ac87830424cb8ecd9ba4a1
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-20 16:45:28 +0900

    First Commit #16

---
+++ ex2-1-5/ex2-1-5/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex2-1-5
+//
+//  Created by ITユーザー on 2016/12/20.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ex2-1-5/ex2-1-5/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ex2-1-5
+//
+//  Created by ITユーザー on 2016/12/20.
+//  Copyright © 2016年 ITユーザー. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
