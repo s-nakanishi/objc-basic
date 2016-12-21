a5f88965e2c96d92f46793ec4116de51c7595f18
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-21 16:03:54 +0900

    Completed. #17

--- ex2-1-6/ex2-1-6/ViewController.m
+++ ex2-1-6/ex2-1-6/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property UIAlertController *alert;
+@property UIAlertController *actionSheet;
+@property (weak, nonatomic) IBOutlet UIWebView *webV;
+- (IBAction)onTap:(id)sender;
 
 @end
 
@@ -17,6 +21,53 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  // アラートダイアログの作成
+  self.alert = [UIAlertController alertControllerWithTitle:@"ネットワークエラー"
+                                                   message:@"ネットワークに接続できません。"
+                                            preferredStyle:UIAlertControllerStyleAlert];
+  
+  UIAlertAction *ok = [UIAlertAction actionWithTitle:@"OK"
+                                               style:UIAlertActionStyleDefault
+                                             handler:^(UIAlertAction *action) {
+                                               NSLog(@"clicked OK Button");
+                                             }];
+  [self.alert addAction:ok];
+
+  // アクションシートの作成
+  self.actionSheet = [UIAlertController alertControllerWithTitle:@"メニュー"
+                                                         message:@""
+                                                  preferredStyle:UIAlertControllerStyleActionSheet];
+  
+  UIAlertAction *goBack = [UIAlertAction actionWithTitle:@"GoBack"
+                                                   style:UIAlertActionStyleDefault
+                                                 handler:^(UIAlertAction *action) {
+                                                   NSLog(@"selected GoBack");
+                                                   [self.webV goBack];
+                                                 }];
+  [self.actionSheet addAction:goBack];
+  
+  UIAlertAction *goForward= [UIAlertAction actionWithTitle:@"GoForward"
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:^(UIAlertAction *action) {
+                                                     NSLog(@"selected GoForward");
+                                                     [self.webV goForward];
+                                                   }];
+  [self.actionSheet addAction:goForward];
+  
+  UIAlertAction *reload = [UIAlertAction actionWithTitle:@"Reload"
+                                                   style:UIAlertActionStyleDefault
+                                                 handler:^(UIAlertAction *action) {
+                                                   NSLog(@"selected Reload");
+                                                   [self.webV reload];
+                                                 }];
+  [self.actionSheet addAction:reload];
+
+  // Webの初期ロード
+  NSString* urlString = @"https://google.com";
+  NSURL* googleURL = [NSURL URLWithString: urlString];
+  NSURLRequest* request = [NSURLRequest requestWithURL: googleURL];
+  [self.webV loadRequest:request];
+  self.webV.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +75,20 @@
   // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+  [self presentViewController:self.actionSheet
+                     animated:YES
+                   completion:^{
+                   }];
+}
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error {
+  if ([error code] != NSURLErrorCancelled) {
+    [self presentViewController:self.alert
+                       animated:YES
+                     completion:^{
+                       NSLog(@"show Alert");
+                     }];
+  }
+}
 @end
