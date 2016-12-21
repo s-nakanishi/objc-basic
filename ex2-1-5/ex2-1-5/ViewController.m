018af9afc1562bdc3063849dbc262e1899d9bdb1
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-21 15:28:49 +0900

    Completed. #16

--- ex2-1-5/ex2-1-5/ViewController.m
+++ ex2-1-5/ex2-1-5/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property UIAlertController *actionSheet;
+- (IBAction)onTap:(id)sender;
 
 @end
 
@@ -17,6 +19,30 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  self.actionSheet = [UIAlertController alertControllerWithTitle:@"メニュー"
+                                                         message:@""
+                                                  preferredStyle:UIAlertControllerStyleActionSheet];
+  
+  UIAlertAction *facebook = [UIAlertAction actionWithTitle:@"Facebook"
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:^(UIAlertAction *action) {
+                                                     NSLog(@"selected Facebook");
+                                                   }];
+  [self.actionSheet addAction:facebook];
+
+  UIAlertAction *twitter = [UIAlertAction actionWithTitle:@"Twitter"
+                                                    style:UIAlertActionStyleDefault
+                                                  handler:^(UIAlertAction *action) {
+                                                    NSLog(@"selected Twitter");
+                                                  }];
+  [self.actionSheet addAction:twitter];
+
+  UIAlertAction *line = [UIAlertAction actionWithTitle:@"Line"
+                                                     style:UIAlertActionStyleDefault
+                                                   handler:^(UIAlertAction *action) {
+                                                     NSLog(@"selected Line");
+                                                   }];
+  [self.actionSheet addAction:line];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +50,10 @@
   // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTap:(id)sender {
+  [self presentViewController:self.actionSheet
+                     animated:YES
+                   completion:^{
+                   }];
+}
 @end
