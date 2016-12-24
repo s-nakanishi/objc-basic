a6d33a6629cecb8c16221e202812783ce978d3ae
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-24 11:58:13 +0900

    First Commit #18

---
+++ ex2-1-7/ex2-1-7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ex2-1-7
+//
+//  Created by 中西　真也 on 2016/12/24.
+//  Copyright © 2016年 Nakanishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
