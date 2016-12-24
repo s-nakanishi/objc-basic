5f7a866fec7886b8ce4359105718f45073bb75e1
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-24 12:58:58 +0900

    Completed. #18

--- ex2-1-7/ex2-1-7/ViewController.m
+++ ex2-1-7/ex2-1-7/ViewController.m
@@ -9,14 +9,40 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *textField;
 
 @end
 
 @implementation ViewController
+- (IBAction)onTap:(id)sender {
+  [self.textField resignFirstResponder];
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string
+{
+  int maxInputLength = 30;
+  
+  NSMutableString *str = [textField.text mutableCopy];
+  
+  [str replaceCharactersInRange:range withString:string];
+  
+  if ([str length] > maxInputLength) {
+    return NO;
+  }
+  
+  return YES;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+  [textField resignFirstResponder];
+  return YES;
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  self.textField.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
