3e668d0149e0ab92118be69c06a62e88de14ab6c
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-24 17:33:47 +0900

    Completed. #20

--- ex2-1-9/ex2-1-9/ViewController.m
+++ ex2-1-9/ex2-1-9/ViewController.m
@@ -9,14 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIDatePicker *picker;
 
 @end
 
 @implementation ViewController
 
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+  UITouch *touch = [[event allTouches] anyObject];
+  if ( touch.view.tag == self.label.tag ) {
+    self.picker.alpha = 1;
+  } else {
+    self.picker.alpha = 0;
+  }
+}
+
+- (IBAction)onDone:(id)sender {
+  self.picker.alpha = 0;
+}
+
+- (void)updateDate:(UIDatePicker *)picker {
+  NSDateFormatter *formatter = [NSDateFormatter new];
+  [formatter setDateFormat:@"yyyy/MM/dd"];
+  NSString *dayStr = [formatter stringFromDate:self.picker.date];
+  self.label.text = dayStr;
+}
+
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  self.label.userInteractionEnabled = YES;
+  self.label.tag = 100;
+  
+  self.picker.alpha = 0;
+  [self.picker addTarget:self action:@selector(updateDate:) forControlEvents:UIControlEventValueChanged];
+  self.picker.date = [NSDate new];
+  [self updateDate:self.picker];
 }
 
 - (void)didReceiveMemoryWarning {
