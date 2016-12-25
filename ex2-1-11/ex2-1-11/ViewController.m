9192e880f1e0242416948c1375cc1fb939044283
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-25 21:33:19 +0900

    Completed. #22

--- ex2-1-11/ex2-1-11/ViewController.m
+++ ex2-1-11/ex2-1-11/ViewController.m
@@ -10,22 +10,34 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *table;
-@property(nonatomic) NSArray *items;
+@property(nonatomic) NSArray *sections;
+@property(nonatomic) NSArray *sectionTitles;
 @end
 
 @implementation ViewController
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+  
+  return self.sections.count;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
+  return self.sectionTitles[section];
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-  return self.items.count;
+  NSArray *items = self.sections[section];
+  return items.count;
 }
 
 - (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
   UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
-  NSString *itemString = NSLocalizedString(self.items[indexPath.row], nil);
+  NSArray *items = self.sections[indexPath.section];
+  NSString *itemString = NSLocalizedString(items[indexPath.row], nil);
   NSArray *arr = [itemString componentsSeparatedByString:@"\n"];
   cell.textLabel.numberOfLines = arr.count;
   cell.textLabel.text =  itemString;
-  cell.imageView.image = [UIImage imageNamed:self.items[indexPath.row]];
+  cell.imageView.image = [UIImage imageNamed:items[indexPath.row]];
   return cell;
 }
 
@@ -35,7 +47,8 @@
   NSBundle *bundle = [NSBundle mainBundle];
   NSString *path = [bundle pathForResource:@"item" ofType:@"plist"];
   NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
-  self.items = dic[@"items"];
+  self.sections = @[dic[@"nigiri"], dic[@"side"]];
+  self.sectionTitles = @[@"にぎりメニュー", @"サイドメニュー"];
   
   self.table.delegate = self;
   self.table.dataSource = self;
