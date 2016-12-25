47ece35204bdf5a6d47c5944649452231079dc72
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-25 16:26:42 +0900

    First Commit #21

---
+++ ex2-1-10/ex2-1-10/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ex2-1-10
+//
+//  Created by 中西　真也 on 2016/12/25.
+//  Copyright © 2016年 Nakanishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
