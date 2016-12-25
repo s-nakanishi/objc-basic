cf5fa43ab7c278df88173d2da1410a5af9315c92
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-25 23:10:18 +0900

    Completed. #23

--- ex2-1-12/ex2-1-12/CollectionViewCell.h
+++ ex2-1-12/ex2-1-12/CollectionViewCell.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CollectionViewCell.h
 //  ex2-1-12
 //
 //  Created by 中西　真也 on 2016/12/25.
@@ -8,8 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController<UICollectionViewDelegate, UICollectionViewDataSource>
-
+@interface CollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
-
--- ex2-1-12/ex2-1-12/ViewController.m
+++ ex2-1-12/ex2-1-12/ViewController.m
@@ -7,16 +7,38 @@
 //
 
 #import "ViewController.h"
+#import "CollectionViewCell.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UICollectionView *collection;
+@property (nonatomic) NSArray *items;
 @end
 
 @implementation ViewController
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+  return self.items.count;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+  CollectionViewCell *cell = [self.collection dequeueReusableCellWithReuseIdentifier:@"customCell" forIndexPath:indexPath];
+  cell.imageView.image = [UIImage imageNamed:self.items[indexPath.row]];
+  return cell;
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  NSBundle *bundle = [NSBundle mainBundle];
+  NSString *path = [bundle pathForResource:@"item" ofType:@"plist"];
+  NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:path];
+  self.items = dic[@"nigiri"];
+  
+  self.collection.delegate = self;
+  self.collection.dataSource = self;
+  UINib *nibFirst = [UINib nibWithNibName:@"CollectionViewCell" bundle:nil];
+  [self.collection registerNib:nibFirst forCellWithReuseIdentifier:@"customCell"];
 }
 
 - (void)didReceiveMemoryWarning {
