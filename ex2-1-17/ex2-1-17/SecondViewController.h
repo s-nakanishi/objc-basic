5653ac38f7cce02b8fc4207268eb7d250fc8e36f
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-29 23:10:33 +0900

    Completed. #28

---
+++ ex2-1-17/ex2-1-17/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ex2-1-17
+//
+//  Created by 中西　真也 on 2016/12/29.
+//  Copyright © 2016年 Nakanishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
