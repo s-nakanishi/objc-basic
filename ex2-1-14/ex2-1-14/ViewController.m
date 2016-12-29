02fdd4251854d092afefa98b979718af65509b7e
s-nakanishi <nakanishi.shinya@it-neec.jp>
2016-12-29 20:24:35 +0900

    First Commit. #25

---
+++ ex2-1-14/ex2-1-14/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ex2-1-14
+//
+//  Created by 中西　真也 on 2016/12/29.
+//  Copyright © 2016年 Nakanishi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
