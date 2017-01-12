39dddcfbb14d2f6811645a5cf686bb8c115b7f6a
s-nakanishi <nakanishi.shinya@it-neec.jp>
2017-01-12 16:37:06 +0900

    Completed. #58

---
+++ ex2-3-2/ex2-3-2/AddViewController.h
@@ -0,0 +1,14 @@
+//
+//  AddViewController.h
+//  ex2-3-2
+//
+//  Created by 中西　真也 on 2017/01/12.
+//  Copyright © 2017年 Nakanishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FMDatabase.h"
+
+@interface AddViewController : UIViewController
+- (void)setFMDatabase:(FMDatabase *)db;
+@end
